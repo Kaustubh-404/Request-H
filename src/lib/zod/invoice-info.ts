f5683211675c9c092e3907a92c7506a6bbb8a330
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-22 12:31:00 +0530

    finished hooks

--- src/lib/zod/invoice-info.ts
+++ src/lib/zod/invoice-info.ts
@@ -1,26 +1,6 @@
 import { z } from 'zod';
 
-export const userInfoSchema = z.object({
-  email: z.string().email().optional(),
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
-  businessName: z.string().optional(),
-  phone: z.string().optional(),
-  address: z
-    .object({
-      street: z.string().optional(),
-      city: z.string().optional(),
-      zipCode: z.string().optional(),
-      country: z.string().optional(),
-      state: z.string().optional(),
-    })
-    .optional(),
-  taxRegistration: z.string().optional(),
-  companyRegistration: z.string().optional(),
-  miscellaneous: z.record(z.any()).optional(),
-});
-
-export const invoiceSchema = z
+export const invoiceInfoSchema = z
   .object({
     meta: z
       .object({ format: z.literal('rnf_invoice'), version: z.literal('0.0.3') })
@@ -31,19 +11,14 @@ export const invoiceSchema = z
     purchaseOrderId: z.string().optional(),
     note: z.string().optional(),
     terms: z.string().optional(),
-    sellerInfo: userInfoSchema
-      .strict()
-      .describe('Seller information')
-      .optional(),
-    buyerInfo: userInfoSchema.strict().describe('Buyer information').optional(),
     invoiceItems: z.array(
       z
         .object({
           name: z.string(),
           reference: z.string().optional(),
           quantity: z.number().gte(0),
-          unitPrice: z.string().regex(/^\d+$/),
-          discount: z.string().regex(/^\d+$/).optional(),
+          unitPrice: z.string(),
+          discount: z.string().optional(),
           tax: z
             .object({
               amount: z.string(),
@@ -72,6 +47,4 @@ export const invoiceSchema = z
   .strict()
   .describe('Request Network Format of an invoice');
 
-export type UserInfo = z.infer<typeof userInfoSchema>;
-export type InvoiceType = z.infer<typeof invoiceSchema>;
-// https://icons.llamao.fi/icons/chains/rsz_rsz_zksync era
+export type InvoiceInfo = z.infer<typeof invoiceInfoSchema>;
